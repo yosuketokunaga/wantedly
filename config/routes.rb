88274d3be9b26fd217c98f3e5e4146c75680caa2
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-09 17:00:58 +0900

    edit routeing
    edit routeing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :companies, controllers: {
+  sessions:      'companies/sessions',
+  passwords:     'companies/passwords',
+  registrations: 'companies/registrations'
+  }
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
+  resources :companies, only: [:index]
+  root "companies#index"
 end
