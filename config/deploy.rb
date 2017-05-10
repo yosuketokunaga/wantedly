260bb72740d6bc7973fd79016856bfc513ef63dd
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-10 20:12:29 +0900

    22
    22

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.8.1'
 
 set :application, 'wantedly'
-set :repo_url, 'git@github.com/yosuketokunaga/wantedly.git'
+set :repo_url, 'git@github.com:yosuketokunaga/wantedly.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
