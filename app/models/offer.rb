e7419c39b15ce28636572fb85b9d5f65cf8da7aa
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-16 13:20:51 +0900

    edit models
    edit models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,9 @@
 class Offer < ApplicationRecord
   mount_uploader :image, ImagesUploader
   paginates_per 5
+
   belongs_to :company
+  has_many :applies
+  has_many :users, through: :applies
+  accepts_nested_attributes_for :users
 end
