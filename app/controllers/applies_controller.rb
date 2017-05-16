b96e918545bd42cebd59a6cb48d9a1a3cc8a49b2
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-16 15:18:13 +0900

    edit controllers
    edit controllers

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -7,8 +7,18 @@ class AppliesController < ApplicationController
 
   def create
     @offer = Offer.find(params[:offer_id])
-    @apply = Apply.new(offer_id: @offer_id, user_id: @user_id)
-    redirect_to root_path, flash: {notice: '応募が完了しました'}
+    @apply = Apply.new(offer_id: apply_params[:offer_id], user_id: current_user.id)
+    if @apply.save
+      redirect_to root_path, flash: {notice: '応募が完了しました'}
+    else
+      flash.now[:alert] = '応募に失敗しました。'
+      redirect_to root_path
+    end
+  end
+
+  private
+  def apply_params
+    params.permit(:user_id, :offer_id)
   end
 
 end
