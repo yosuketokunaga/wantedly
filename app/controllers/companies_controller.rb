7f04192a395b04f05799ace552cc94ff863ce274
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-16 19:44:18 +0900

    company側のshowのview作成
    company側のshowのview作成

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,9 @@ class CompaniesController < ApplicationController
   def index
     @company = Company.new
   end
+
+  def show
+    @company = Company.find(params[:id])
+    # @applies = Applies.where(user_id: @user.id)
+  end
 end
