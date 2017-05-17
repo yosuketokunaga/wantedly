a44afcbb1e8a6af6a745fc39065367afed4b1da9
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-05-17 19:42:36 +0900

    edit controllers
    edit controllers

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,6 +6,5 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
     @offers = Offer.where(company_id: @company.id)
-    @applies = Apply.where(company_id: @company.id)
   end
 end
